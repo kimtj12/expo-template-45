4dabecca1a5a8c94e2ca88a473d5f70e25481841
kimtj12 <kimtj@me.com>
2022-06-02 09:03:42 +0900

    added base screen

---
+++ src/screens/_baseStack/index.js
@@ -0,0 +1,38 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { useTheme } from "native-base";
+import DevPlaceholderScreen from "../../components/navigation/DevPlaceholderScreen";
+
+const Stack = createStackNavigator();
+const StackNavigator = () => {
+  const { colors } = useTheme();
+
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen
+        name="HomeScreen"
+        component={DevPlaceholderScreen}
+        options={{
+          title: "신규 견적",
+          headerShown: true,
+          headerTitleAlign: "left",
+          headerTitleStyle: {
+            color: colors.primary["500"],
+            fontWeight: "bold",
+            fontFamily: "IBMPlex_SemiBold",
+          },
+          cardStyle: {
+            backgroundColor: "#f8f9fa",
+          },
+        }}
+      />
+      {/* <AuthStack.Screen name="Register" component={Register} /> */}
+      {/* <AuthStack.Screen name="ForgetPassword" component={ForgetPassword} /> */}
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
